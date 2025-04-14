ff868a8d63caac0b8a176143dc78a86ac8e68194
StevenJ <ezazhel@gmail.com>
2025-04-14 15:37:14 +0200

    feat(grid): use full tailwind for grid component, it is not as reusable but it remove some calculation and use tailwind breakpoint

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,15 +1,8 @@
 import {FC} from "react";
 import {GridProps} from "./type.ts";
 
-export const Grid: FC<GridProps> = ({className, children, gap = 1, columns = 4 }) => {
-	//tailwind removing unknown declaration of className on build we need to manage style the old way.
-	const style = {
-		gap: `${gap}rem`,
-		display:'grid',
-		maxWidth: `${1024+16*gap*columns}px`, //1 rem = 16px
-		gridTemplateColumns: `repeat(auto-fit, minmax(calc(${100/ columns}% - ${gap}rem), 1fr))` // using the RAM pattern of grid we have a responsive grid.
-	}
-	return <div className={className} style={style}>
+export const Grid: FC<GridProps> = ({className, children }) => {
+	return <div className={'max-w-[calc(1024px+(16px*3))] w-fit grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 '+className}>
 		{children}
 	</div>
 }
--- src/components/Grid/type.ts
+++ src/components/Grid/type.ts
@@ -2,7 +2,5 @@ import {ReactNode} from "react";
 
 export type GridProps = {
 	children: ReactNode;
-	columns?: number;
-	gap?: number; //in rem 1 rem = 16px btw
 	className?: string;
 }
